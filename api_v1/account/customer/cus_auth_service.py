d66e4e69521ea881c7c1238dcd233da37f131f30
unknown <dev.cslee@gmail.com>
2019-08-25 07:42:32 +0900

    [modify] login logger

--- api_v1/account/customer/cus_auth_service.py
+++ api_v1/account/customer/cus_auth_service.py
@@ -55,23 +55,31 @@ class CusAuthService:
     @classmethod
     def login(cls, email, pwd):
         # 아이디, pwd 확인
+        cls.logger.info("login 진입")
         with session_scope() as session:
             customer = session.query(Customer).filter(Customer.email == email).first()
+            cls.logger.info("{} 조회성공".format(customer.email))
+
             if customer is None:
                 response = set_response("99", {"errorMsg": "존재하지 않는 이메일입니다."})
                 return response
-            cid = customer.cid
-            cus_pwd = customer.password
-            salt = customer.salt
-            pwd = AccountUtils.set_pwd(pwd, salt)
-            isauth = AccountUtils.chk_pwd(pwd, cus_pwd)
-            if isauth:
-                cus = {
-                    'email': email,
-                    'uid': cid
-                }
-                access_token = AccountUtils.get_token(cus)
-                response = set_response("00", {"token": access_token})
+            try:
+                cid = customer.cid
+                cus_pwd = customer.password
+                salt = customer.salt
+                pwd = AccountUtils.set_pwd(pwd, salt)
+                isauth = AccountUtils.chk_pwd(pwd, cus_pwd)
+                if isauth:
+                    cus = {
+                        'email': email,
+                        'uid': cid
+                    }
+                    access_token = AccountUtils.get_token(cus)
+                    response = set_response("00", {"token": access_token})
+            except BaseException as e:
+                cls.logger.warning("크리티컬 이슈")
+                cls.logger.warning(e)
+                response = set_response("99", {"errorMsg": '토큰 생성 실패'})
             else:
                 response = set_response("99", {"errorMsg": '회원정보가 틀렸습니다.'})
         return response
