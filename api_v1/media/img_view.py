3ae3f24a7fd41e5f697ab1eb787bd1d04ef95150
unknown <dev.cslee@gmail.com>
2019-08-25 08:55:44 +0900

    [modify] media logging

--- api_v1/media/img_view.py
+++ api_v1/media/img_view.py
@@ -3,14 +3,20 @@ import os
 from flask_restful import Resource
 from flask import send_file, abort
 from urllib.parse import quote
+from log import create_logger
 
 class ImgProcess(Resource):
+    logger = create_logger(__name__)
+
     def get(self, img_type, img_date, filename):
         img_path = os.path.join("images", img_type, img_date, filename)
         # /images/<string:img_type>/<string:img_date>/<string:filename>'
         attachement_name = quote(filename)
         isfile = os.path.isfile(img_path)
+        self.logger.info(img_path)
+        self.logger.info(isfile)
         if not isfile:
             return abort(404)
+
         return send_file(img_path, attachment_filename=attachement_name)
 
